136886551b2058cc3b974122824ab770daf51d82
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-10 22:01:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
   attr_accessor :owner, :mood
-  attr_reader :name
+  attr_reader :name, :specics
   
   @@all = []
   
   def initialize(name,owner)
+    
     @name = name
     @owner = owner
     @mood = "nervous"
